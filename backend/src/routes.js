100935f0cdc252f1eda44cab8bf2f98857016393
RennanD <rplaykl@gmail.com>
2020-03-24 10:27:12 -0300

    feat: Create a ONG, List all ONGs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+import OngController from './app/controllers/OngController';
+
 const routes = new Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
 
 export default routes;
